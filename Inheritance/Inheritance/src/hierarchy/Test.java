f6308e0e65e9b534253e93fe2fdaa95dc6f1fb79
saurabhc12 <saurabhchavhan334@gmail.com>
2024-12-14 16:29:06 +0530

    Operations on Inheritance

--- Inheritance/Inheritance/src/hierarchy/Test.java
+++ Inheritance/Inheritance/src/hierarchy/Test.java
@@ -2,22 +2,22 @@ package hierarchy;
 
 public class Test {
 	public static void main(String[] args) {
-//		Calculator c1=new Calculator();
-//		//System.out.println(c1);
-//		Calculator c2=new Calculator();
-//		Calculator c3=new Calculator();
-//		c1.average(15,15);
-//		c2.average(10,15);
-//		c3.average(17,15); 
+		Calculator c1=new Calculator();
+		//System.out.println(c1);
+		Calculator c2=new Calculator();
+		Calculator c3=new Calculator();
+		c1.average(15,15);
+		c2.average(10,15);
+		c3.average(17,15); 
 		
-//		A a1=new A();
-//		a1.m1();
+		A a1=new A();
+		a1.m1();
 		
-//		C jay= new C();
-//		jay.marry();
+		C jay= new C();
+		jay.marry();
 
-		//Student s1=new Student(1,"jay");
-		//Student s1=new Student("jay","pune");
+		Student s1=new Student(1,"jay");
+		Student s2=new Student("jay","pune");
 		
 		P p1=new P();
 		System.out.println(p1.hashCode());
